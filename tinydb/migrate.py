4449adcd3919ff67142e3ea58ba38994b0bce538
Markus Siemens <siemens1993@gmail.com>
2014-09-02 13:02:00 +0200

    Added docs for migration with custom storage
    [skip ci]

--- tinydb/migrate.py
+++ tinydb/migrate.py
@@ -6,10 +6,24 @@ from tinydb.storages import JSONStorage
 
 def migrate(*args, **kwargs):
     """
-    :type db: tinydb.TinyDB
+    Migrate a database to the scheme used in v2.0.
+
+    To migrate a db that uses a custom storage, use
+
+    >>> from tinydb.migrate import migrate
+    >>> args = [...]  # args for your storage class
+    >>> kwargs = {...}  # kwargs for your storage class
+    >>> migrate(*args, **kwargs, storage=YourStorageClass)
+    True
+
+    :param storage: The class of the storage to use. Will be initialized
+                    with ``args`` and ``kwargs``. Default:
+                    :class:`~tinydb.storages.JSONStorage`
+    :returns: ``True`` if the db has been migrated, ``False`` if the db
+              didn't need a migration.
     """
     storage_cls = kwargs.pop('storage', JSONStorage)
-    storage = storage_cls(*args)
+    storage = storage_cls(*args, **kwargs)
 
     new_data = {}
     data = storage.read()
