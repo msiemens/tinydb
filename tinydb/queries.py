4e824356c1bb3e6d44125a529791a6a1f76ca51f
Markus Siemens <siemens1993@gmail.com>
2015-09-17 17:26:42 +0200

    Fix for Python 2.6

--- tinydb/queries.py
+++ tinydb/queries.py
@@ -48,7 +48,7 @@ class QueryImpl(object):
         return hash(self.hashval)
 
     def __repr__(self):
-        return 'Query{}'.format(self.hashval)
+        return 'Query{0}'.format(self.hashval)
 
     def __eq__(self, other):
         return self.hashval == other.hashval
