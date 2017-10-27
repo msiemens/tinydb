26e916487c9dcff5b2e0730e6a7e2e12e17f0648
Andrew Shapton <alshapton@gmail.com>
2017-10-27 22:22:07 +0000

    Attempting to finalise 100% code overage

--- tinydb/database.py
+++ tinydb/database.py
@@ -523,10 +523,15 @@ class Table(object):
                        return mult * result
                     else:
                        return 0
+
+            return sorted(items, cmp=comparer) if sys.version_info[0] < 3 else sorted(items, key=functools.cmp_to_key(comparer))
+
+            """
             if sys.version_info[0] < 3:
                 return sorted(items, cmp=comparer)
             else:
                 return sorted(items, key=functools.cmp_to_key(comparer))
+            """
 
         if cond in self._query_cache:
             return _multikeysort(self._query_cache[cond][:],sortkeys)
