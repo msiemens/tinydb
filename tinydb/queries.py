3786a80229273959df4e7fde2703e7486cefff30
Ruan Comelli <ruancomelli@gmail.com>
2023-09-03 15:43:11 -0300

    refactor(queries): always import `Protocol` from `typing`

--- tinydb/queries.py
+++ tinydb/queries.py
@@ -17,15 +17,9 @@ False
 """
 
 import re
-import sys
-from typing import Mapping, Tuple, Callable, Any, Union, List, Optional
+from typing import Mapping, Tuple, Callable, Any, Union, List, Optional, Protocol
 
 from .utils import freeze
-
-if sys.version_info >= (3, 8):
-    from typing import Protocol
-else:
-    from typing_extensions import Protocol
 
 __all__ = ('Query', 'QueryLike', 'where')
 
